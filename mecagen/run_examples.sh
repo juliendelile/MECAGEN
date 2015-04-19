ac4441051a28da2479c10ba093b16ec162616935
Julien <julien.delile@gmail.com>
2015-04-19 21:34:33 +0100

    Add simulations' last timestep in run_examples.sh

--- mecagen/run_examples.sh
+++ mecagen/run_examples.sh
@@ -14,15 +14,15 @@ select opt in "${options[@]}"
 do
     case $opt in
         "$option1")
-            ./launchGUI.sh -param examples/default/SignalMedToggleSwitch_FlatTissue_grn_default/param/param_archive.xml -metaparam examples/default/SignalMedToggleSwitch_FlatTissue_grn_default/state/metaparam_archive.xml -state examples/default/SignalMedToggleSwitch_FlatTissue_grn_default/state/state_archive.xml
+            ./launchGUI.sh -param examples/default/SignalMedToggleSwitch_FlatTissue_grn_default/param/param_archive.xml -metaparam examples/default/SignalMedToggleSwitch_FlatTissue_grn_default/state/metaparam_archive.xml -state examples/default/SignalMedToggleSwitch_FlatTissue_grn_default/state/state_archive.xml -te 1000
             break
             ;;
         "$option2")
-            ./launchGUI.sh -param examples/default/Boundaryformation_epithelialization/param/param_archive.xml -metaparam examples/default/Boundaryformation_epithelialization/state/metaparam_archive.xml -state examples/default/Boundaryformation_epithelialization/state/state_archive.xml
+            ./launchGUI.sh -param examples/default/Boundaryformation_epithelialization/param/param_archive.xml -metaparam examples/default/Boundaryformation_epithelialization/state/metaparam_archive.xml -state examples/default/Boundaryformation_epithelialization/state/state_archive.xml -te 800
             break
             ;;
         "$option3")
-            ./launchGUI.sh -param examples/zebrafish/Zebrafish_epiboly/param_archive.xml -metaparam examples/zebrafish/Zebrafish_epiboly/metaparam_archive.xml -state examples/zebrafish/Zebrafish_epiboly/state_archive.xml
+            ./launchGUI.sh -param examples/zebrafish/Zebrafish_epiboly/param_archive.xml -metaparam examples/zebrafish/Zebrafish_epiboly/metaparam_archive.xml -state examples/zebrafish/Zebrafish_epiboly/state_archive.xml -te 4000
             break
             ;;
         "Quit")
